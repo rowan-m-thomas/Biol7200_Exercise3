bace3f1dceaf281d5157f20eb666d18c9c84fb53
Rowan <rowan278@gmail.com>
2026-09-21 20:23:53 -0400

    Add identity and length filter

--- find_homologs.sh
+++ find_homologs.sh
@@ -8,6 +8,6 @@ echo "Query: $query"
 echo "Subject: $subject"
 echo "Output: $output"
 
-tblastn -query "$query" -subject "$subject" -outfmt '6 std qlen' > $output
+tblastn -query "$query" -subject "$subject" -outfmt '6 std qlen' | awk '$3 > 30 && $4 > 0.9 * $13' > "$output"
 
 
